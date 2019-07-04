d553d5cb246b60114ad1bafb59638f70c472bd0b
jasonleewilson <info@jasonleewilson.com>
2019-07-04 01:25:48 -0700

    updafed Clock.jsx

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -8,8 +8,7 @@ class Clock extends Component {
             days: 0,
             hours: 0,
             minutes: 0,
-            seconds: 0,
-            milseconds: 0
+            seconds: 0
         }
     }
 
